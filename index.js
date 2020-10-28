423abe718bf4e2cc6ff4de28e6d133f6cbdd9f49
mrTuankiet <hung.buma@gmail.com>
2020-10-28 20:39:57 +0700

    update response.send

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var port = 3000;
 
 
+app.get('/', function(request, response){
+        response.send('Hello coderx.tokyo');
+});
+
 app.listen(port, function(){
     console.log('hello express');
 });
